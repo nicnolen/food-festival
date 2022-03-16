f7715444de8f9bbb81960c4f308ef1ecfb00423a
Nicholas Nolen <nicnolen@ymail.com>
2022-03-16 12:59:48 -0500

    activated the service worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -16,15 +16,39 @@ const FILES_TO_CACHE = [
   './dist/schedule.bundle.js',
 ];
 
-// Cache resources
+// Cache resources and install the service worker
 self.addEventListener('install', function (e) {
   // wait until the work is complete before terminating the service worker.
   e.waitUntil(
-      // find specific cache by name
-      caches.open(CACHE_NAME).then(function (cache) {
-          console.log('installing cache : ' + CACHE_NAME)
-          // add every file from FTC array to the cache
-          return cache.addAll(FILES_TO_CACHE)
-      })
-  )
+    // find specific cache by name
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME);
+      // add every file from FTC array to the cache
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
 }); // service workers run before the window object is created so we use the self keyword to instantiate listeners on the service worker
+
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create keeplist
+      let cacheKeeplist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX);
+      });
+      // add current cache name to keeplist
+      cacheKeeplist.push(CACHE_NAME);
+
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log('deleting cache : ' + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
+    })
+  );
+});
